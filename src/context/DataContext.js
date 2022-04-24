734d5b7d19035a69b37700d0e54bb602dfb6a3f7
MateusP21 <mateuspasso@outlook.com>
2022-04-24 20:58:34 -0300

    requisito 4

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -6,6 +6,7 @@ export const DataContext = createContext({});
 
 export const DataProvider = ({ children }) => {
   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
   // const [selectedFilter, setSelectedFilter] = useState('byColumnAndValues');
   const [filter, setFilter] = useState({
     filterByName: {
@@ -13,11 +14,7 @@ export const DataProvider = ({ children }) => {
     },
     selectedFilter: '',
     filterByNumericValues: [
-      {
-        column: '',
-        comparison: '',
-        value: '0',
-      },
+
     ],
   });
   useEffect(() => {
@@ -25,13 +22,19 @@ export const DataProvider = ({ children }) => {
       const res = await fetch(ENDPOINT);
       const json = await res.json();
       setData(json.results);
+      setFilteredData(json.results);
     };
     fetchData();
   }, []);
 
-  const filterByPlanetName = (planet) => planet.name
-    .toLowerCase()
-    .includes(filter.filterByName.name);
+  const filterByPlanetName = (currentData) => {
+    const { filterByName: { name } } = filter;
+    const result = currentData.filter(
+      (planet) => planet.name.toLowerCase().includes(name),
+    );
+
+    return result;
+  };
 
   const handleSearch = (filterValue) => {
     setFilter((prevState) => ({
@@ -49,33 +52,36 @@ export const DataProvider = ({ children }) => {
     }));
   };
 
-  const filterByColumnAndValues = (planet) => {
+  const filterByColumnAndValues = (currentData) => {
     const { filterByNumericValues } = filter;
     let selectedPlanet;
     const greaterThen = 'maior que';
     const lessThen = 'menor que';
     const equalThen = 'igual a';
-
+    let result;
+    let temp = [...currentData];
     filterByNumericValues.forEach((currentFilter) => {
       const { column, comparison, value } = currentFilter;
+      result = temp.filter((planet) => {
+        const filterOptions = {
+          [equalThen]: planet[column] === (value) && planet,
+          [lessThen]: planet[column] < Number(value) && planet,
+          [greaterThen]: planet[column] > Number(value) && planet,
+        };
 
-      const filterOptions = {
-        [equalThen]: planet[column] === (value) && planet,
-        [lessThen]: planet[column] < Number(value) && planet,
-        [greaterThen]: planet[column] > Number(value) && planet,
-      };
-
-      selectedPlanet = filterOptions[comparison];
+        selectedPlanet = filterOptions[comparison];
+        return selectedPlanet;
+      });
+      temp = result;
     });
-
-    return selectedPlanet === planet && planet;
+    return temp;
   };
 
   const handleData = (type) => {
     if (!type) return data;
     const filterList = {
-      byName: data.filter(filterByPlanetName),
-      byColumnAndValues: data.filter(filterByColumnAndValues),
+      byName: filterByPlanetName(filteredData),
+      byColumnAndValues: filterByColumnAndValues(filteredData),
     };
 
     return filterList[type];
@@ -87,7 +93,7 @@ export const DataProvider = ({ children }) => {
         handleData,
         handleSearch,
         filter,
-        // handleFilter,
+
         filterByPlanetName,
         filterByColumnAndValues,
         submitFilter,
